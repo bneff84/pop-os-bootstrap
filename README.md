be71475e09a13bee0e8b4464af974b33d85860b5
Brian Neff <bneff84@gmail.com>
2019-10-05 18:00:08 -0400

    Initial commit of bootstrap code
    This is the current working bootstrap code from Pop!_OS 19.04

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# pop-os-bootstrap
-Bootstrap project to make setting up a new development environment on Pop!_OS easier.
+#Pop_OS! 19.04 Bootstrap Project
+ 
+This repo contains a few files intended to make setting up a brand new development environment on Pop_OS! 19.04 quick
+and easy.
+
+I work primarily as a Magento developer. So this bootstrap may include some software (like the Magento Cloud CLI Tool)
+that you don't need. Feel free to customize this to your liking, and happy coding!
+
+## Installation
+
+To use, just install Pop!_OS 19.04, clone the repo to a local folder, and run the `bootstrap.sh` script from the command
+line. The bootstrap script will automatically download and configure software to get you up and running quickly.
+* [Valet Linux](https://github.com/cpriego/valet-linux) - Lightning fast, low-memory local development using PHP/Nginx
+* [OpenConnect 8](https://github.com/dlenski/openconnect) - Command-line VPN Utility with GlobalProtect Support
+* [DBeaver](https://dbeaver.io/) - Database Management GUI for many popular databases
+* [Magento Cloud CLI Tool](https://devdocs.magento.com/guides/v2.3/cloud/before/before-workspace-magento-prereqs.html#cloud-ssh-cli-cli-install) - Used for working on Magento Cloud Projects
+* MySQL Server - SQL-based RDBMS
+* Git - Version Control
+* Composer - Package Manager for PHP Projects
+* PHP 7.3
+* PHP 7.1
+* PHP 5.6
+
+## Changing PHP Versions
+To change PHP versions, use the Bash Alias `php-version` which will allow you to easily swap between PHP versions for
+both valet and CLI. If you ever need to change **just** the CLI version for any reason, you can do this with the
+`sudo update-alternatives --set php /usr/bin/php7.1` command. Just swap out `7.1` for the version you'd like to use
+(it has to already be installed).
+
+## Connecting to a GlobalProtect VPN with web-based authentication
+I connect to a GlobalProtect VPN for my current job, so this bootstrap script includes some setup to allow this to work
+on Pop!_OS for me out of the box. If you use a GlobalProtect VPN with web-based authentication, this **should** work for
+you as well, but I've only tested it with my own personal use case.
+
+To connect to the VPN, open a new terminal window and run the `vpn-connect` bash alias created by the bootstrap script.
+This will open a WebKit headless browser window via the `gp-saml-gui.py` [helper script](https://github.com/dlenski/gp-saml-gui)
+, which will allow you to authenticate using web-based auth. The helper will grab the authentication cookie returned by your
+web-based sign-on provider and pass it to the OpenConnect CLI call to log you into the VPN.
+
+The `vpnc-script` included has been altered from the version packaged with OpenConnect, and should be used. It resolves
+a compatibility issue that prevented the VPN's DNS servers from taking precedence over the ones defined by the local
+network.
+
+## Recommended Software
+While I've included most of the "major" software, there are some packages that are not automatically installed, but
+are very helpful programs to improve the development experience. Pop_OS! Sits on top of Ubuntu, which is Debian based.
+So in general, any `.deb` package installer **should** work and any software you may use on Ubuntu **should** be
+compatible.
+* [GitKraken](https://www.gitkraken.com/) - Git GUI, Merge/Diff Tool, Repo Browser
+* [JetBrains Toolbox](https://www.jetbrains.com/toolbox-app/) - Simple Widget-like Utility for managing/installing
+JetBrains software like PHPStorm and other IntelliJ IDEA Platform tools
+* [Google Chrome](https://www.google.com/chrome/) - Google's Web Browser based on Chromium - Don't forget you can
+"install" many web-applications built using PWA architecture (like Office 365/Outlook) so they run like native apps
+* [Slack](https://slack.com/downloads/linux) - Work chat for your whole team!
+* Pop!_Shop - Basically a GUI for the command line package manager. Allows you to browse available software and install
+or remove programs. Definitely worth a look for quick applications you might need access to without downloading a `.deb`
+installer.
+
+## Useful Tips
+### Keyboard Shortcuts
+Pop! comes with a lot of helpful keyboard shortcuts built in right off the bat. Many of these provide similar
+functionality to that of a certain fruit-based computer. An up-to-date listing of these can always be found on the
+[Pop!_OS Website](https://pop.system76.com/docs/keyboard-shortcuts/) but some of the more common ones are outlined here.
+Your "super" key is the "Windows" key on most US keyboards.
+* `Tap the Super Key` - Activities Overview - Shows thumbnails of all open windows and allows global search
+* `Super + V` - Open/Close Calendar/Notifications Tray
+* `Super + T` - Open a new Terminal window
+* `Super + Up Arrow`/`Super + Down Arrow` - Change your virtual desktop workspace
+* `Ctrl + Super + Up Arrow` - Maximize/Restore current window on current workspace
+* `Ctrl + Super + Left Arrow`/`Ctrl + Super + Right Arrow` - Move window to left/right half of the current workspace
+* `Ctrl + Super + Shift + Left Arrow`/`Ctrl + Super + Shift + Right Arrow` - Move window left/right to the next display
+(multiple monitors)
---
+++ bootstrap.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+#get the current directory of the script
+BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+#prompt user for sudo as we're going to need it
+sudo echo "
+                                                   ///////////////
+                                             */////////////////////////*
+                                          /////////////////////////////////
+                                       ///////////////////////////////////////
+                                     /////////,           //////////////////////
+                                   ////////*                */////////////////////
+                                  ///////                     /////////////////////
+                                .///////         ////         ./////////////////////.
+                               *////////*        /////.        //////////////////////,
+                               //////////*        /////        /////.     ////////////
+                              ////////////*        ////*       ////.        ///////////
+                             //////////////*        ///       /////        *////////////
+                             ///////////////*                //////        /////////////
+                             /////////////////             ////////       //////////////
+                             //////////////////         ,/////////.      ///////////////
+                             ///////////////////       *//////////     .////////////////
+                             ////////////////////       //////////    //////////////////
+                             /////////////////////       /////////   ///////////////////
+                             //////////////////////       //////////////////////////////
+                              //////////////////////      ////////,////////////////////
+                               //////////////////////,     /////    //////////////////
+                               *///////////////////////   //////,   /////////////////,
+                                ,///////////////////////////////////////////////////,
+                                  /////////                               /////////
+                                   ////////                               ////////
+                                     ///////*,,,,,,,,,,,,,,,,,,,,,,,,,,,*///////
+                                       ///////////////////////////////////////
+                                          /////////////////////////////////
+                                             */////////////////////////*
+
+       ____                ___  ____  _   _  ___   ___  _  _     ____              _       _
+      |  _ \ ___  _ __    / _ \/ ___|| | / |/ _ \ / _ \| || |   | __ )  ___   ___ | |_ ___| |_ _ __ __ _ _ __
+      | |_) / _ \| '_ \  | | | \___ \| | | | (_) | | | | || |_  |  _ \ / _ \ / _ \| __/ __| __| '__/ _\` | '_ \\
+      |  __/ (_) | |_) | | |_| |___) |_| | |\__, | |_| |__   _| | |_) | (_) | (_) | |_\__ \ |_| | | (_| | |_) |
+      |_|   \___/| .__/___\___/|____/(_) |_|  /_(_)___/   |_|   |____/ \___/ \___/ \__|___/\__|_|  \__,_| .__/
+                 |_| |_____|                                                                            |_|
+
+This is NOT an un-attended installation script. The majority of the installation can continue without your
+input, but there may be action needed by you to confirm sudo access along the way. With a good internet connection
+this process should only take a few minutes to complete. Please sit tight. Your patience will be rewarded!"
+read -p "Press [enter] to continue or ctrl-c to exit"
+
+#add the lid state switch to kernelstub so we don't get stuck in a suspend loop after closing/opening the laptop lid
+sudo kernelstub -a button.lid_init_state=open
+
+#add custom repo for older versions of php
+sudo add-apt-repository ppa:ondrej/php -y
+
+# remove preinstalled stuff we don't need (aka apache) and cleanup any unused packages
+sudo apt -y remove apache2
+sudo apt -y autoremove
+
+#run an update of repos and upgrade any existing software with available upgrades before continuing
+sudo apt update
+sudo apt -y upgrade
+
+#install all required software
+#install mysql server, git, composer, dbeaver (DB GUI) and dependencies for valet-linux
+sudo apt -y install git jq xsel libnss3-tools composer mysql-server dbeaver-ce
+#install php 7.3
+sudo apt -y install php7.3 php7.3-fpm php7.3-cli php7.3-common php7.3-curl php7.3-gd php7.3-bcmath php7.3-xml php7.3-mbstring php7.3-xml php7.3-xmlrpc php7.3-mysql php7.3-soap php7.3-intl php7.3-ldap php7.3-curl
+#php 7.2 causes errors in valet-linux at the time of the creating of this tool, hence it not being included -- install at your own risk
+#sudo apt -y install php7.2 php7.2-fpm php7.2-cli php7.2-common php7.2-curl php7.2-gd php7.2-bcmath php7.2-xml php7.2-mbstring php7.2-xml php7.2-xmlrpc php7.2-mysql php7.2-soap php7.2-intl php7.2-ldap php7.2-curl
+#install php 7.1
+sudo apt -y install php7.1 php7.1-fpm php7.1-cli php7.1-common php7.1-curl php7.1-gd php7.1-bcmath php7.1-dom php7.1-mbstring php7.1-xml php7.1-xmlrpc php7.1-zip php7.1-mysql php7.1-soap php7.1-intl php7.1-mcrypt php7.1-ldap php7.1-curl php7.1-mcrypt
+#install php 5.6
+sudo apt -y install php5.6 php5.6-fpm php5.6-cli php5.6-common php5.6-curl php5.6-gd php5.6-bcmath php5.6-dom php5.6-mbstring php5.6-xml php5.6-xmlrpc php5.6-zip php5.6-mysql php5.6-soap php5.6-intl php5.6-mcrypt php5.6-ldap php5.6-curl php5.6-mcrypt
+
+#set PHP 7.1 as the active version as this is what valet-linux prefers to use at the moment
+sudo update-alternatives --set php /usr/bin/php7.1
+
+#install Magento Cloud CLI Tool
+curl -sS https://accounts.magento.cloud/cli/installer | php
+
+#install openconnect and the utility to use it with GP OKTA based authentication
+sudo apt -y install gir1.2-webkit2-4.0 openconnect
+
+#setup the user in mysql to work from anywhere with no password for easy access
+sudo mysql -e "create user '$USER'@'%'; grant all on *.* to '$USER'@'%' with grant option; flush privileges;"
+
+#install valet linux
+composer global require cpriego/valet-linux
+
+#run valet install and make a Code folder for storing project directories
+valet install
+mkdir ~/Code
+
+#create our bash alias for changing PHP versions and connecting to the vpn
+read -r -d '' BASH_ALIASES <<EOF
+php-version () {
+    if [ -z "\$1" ]; then
+        echo "Usage: php-version <7.3|7.1|5.6>"
+        echo "       Changes the current PHP version used by the system and valet-linux at the same time."
+        php --version
+        return 1
+    fi
+    sudo valet use \$1
+    sudo update-alternatives --set php "/usr/bin/php\$1"
+}
+
+vpn-connect () {
+    ${BASEDIR}/vpn-connect \$1
+}
+EOF
+echo "$BASH_ALIASES" > ~/.bash_aliases
+
+#reload bash
+source ~/.bashrc
+
+echo "
+                                           ____                   _
+                                          |  _ \  ___  _ __   ___| |
+                                          | | | |/ _ \| '_ \ / _ \ |
+                                          | |_| | (_) | | | |  __/_|
+                                          |____/ \___/|_| |_|\___(_)"
+echo "
+                Please restart the system to make sure all software is completely initialized.
+
+    Check out the README.md file for helpful tips, software, and instructions on how to use the built-in aliases."
