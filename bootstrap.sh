345dd5096b4bcacde5089edfd2c407dac120f5d4
Brian Neff <bneff84@gmail.com>
2020-05-08 09:25:59 -0400

    feature/20.04
    * Added Postman to gitignore until we can figure out why it's creating its folder outside vendor
    * Added known bugs section to readme to track known bugs
    * Added wait timeouts to the nginx config to extend the normal timeout

--- bootstrap.sh
+++ bootstrap.sh
@@ -76,10 +76,12 @@ sudo apt -y install git jq xsel libnss3-tools dbeaver-ce evolution evolution-ews
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 #install php 7.3
 sudo apt -y install php7.3 php7.3-fpm php7.3-cli php7.3-common php7.3-curl php7.3-gd php7.3-bcmath php7.3-xml php7.3-mbstring php7.3-xml php7.3-xmlrpc php7.3-zip php7.3-mysql php7.3-soap php7.3-intl php7.3-ldap php7.3-curl
-#php 7.2 causes errors in valet-linux at the time of the creating of this tool, hence it not being included -- install at your own risk
+#install php 7.2
 sudo apt -y install php7.2 php7.2-fpm php7.2-cli php7.2-common php7.2-curl php7.2-gd php7.2-bcmath php7.2-xml php7.2-mbstring php7.2-xml php7.2-xmlrpc php7.2-zip php7.2-mysql php7.2-soap php7.2-intl php7.2-ldap php7.2-curl
 #install php 7.1
 sudo apt -y install php7.1 php7.1-fpm php7.1-cli php7.1-common php7.1-curl php7.1-gd php7.1-bcmath php7.1-dom php7.1-mbstring php7.1-xml php7.1-xmlrpc php7.1-zip php7.1-mysql php7.1-soap php7.1-intl php7.1-mcrypt php7.1-ldap php7.1-curl php7.1-mcrypt
+#install php 7.0
+sudo apt -y install php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-curl php7.0-gd php7.0-bcmath php7.0-dom php7.0-mbstring php7.0-xml php7.0-xmlrpc php7.0-zip php7.0-mysql php7.0-soap php7.0-intl php7.0-mcrypt php7.0-ldap php7.0-curl php7.0-mcrypt
 #install php 5.6
 sudo apt -y install php5.6 php5.6-fpm php5.6-cli php5.6-common php5.6-curl php5.6-gd php5.6-bcmath php5.6-dom php5.6-mbstring php5.6-xml php5.6-xmlrpc php5.6-zip php5.6-mysql php5.6-soap php5.6-intl php5.6-mcrypt php5.6-ldap php5.6-curl php5.6-mcrypt
 
@@ -102,13 +104,14 @@ xdebug.remote_port=9000
 xdebug.remote_autostart=1
 xdebug.idekey=PHPSTORM
 EOF
-sudo echo "$XDEBUG_CONFIG" | sudo tee -a /etc/php/7.3/mods-available/xdebug.ini
-sudo echo "$XDEBUG_CONFIG" | sudo tee -a /etc/php/7.2/mods-available/xdebug.ini
-sudo echo "$XDEBUG_CONFIG" | sudo tee -a /etc/php/7.1/mods-available/xdebug.ini
-sudo echo "$XDEBUG_CONFIG" | sudo tee -a /etc/php/5.6/mods-available/xdebug.ini
+sudo echo "$XDEBUG_CONFIG" | sudo tee /etc/php/7.3/mods-available/xdebug.ini
+sudo echo "$XDEBUG_CONFIG" | sudo tee /etc/php/7.2/mods-available/xdebug.ini
+sudo echo "$XDEBUG_CONFIG" | sudo tee /etc/php/7.1/mods-available/xdebug.ini
+sudo echo "$XDEBUG_CONFIG" | sudo tee /etc/php/7.0/mods-available/xdebug.ini
+sudo echo "$XDEBUG_CONFIG" | sudo tee /etc/php/5.6/mods-available/xdebug.ini
 
-#set PHP 7.1 as the active version as this is what valet-linux prefers to use at the moment
-sudo update-alternatives --set php /usr/bin/php7.1
+#set PHP 7.2 as the active version as this is what valet-linux prefers to use at the moment
+sudo update-alternatives --set php /usr/bin/php7.2
 
 #install Magento Cloud CLI Tool
 curl -sS https://accounts.magento.cloud/cli/installer | php
@@ -186,9 +189,14 @@ mkdir ~/Code
 sudo rm -f /usr/local/bin/valet
 sudo ln -s "$BASEDIR"/helpers/valet /usr/local/bin/valet
 
-#add a default nginx config file for valet to increase fastcgi buffer size
+#add a default nginx config file for valet to increase fastcgi buffer size and timeouts
 echo "fastcgi_buffers 16 16k;
-fastcgi_buffer_size 32k;" > ~/.valet/Nginx/00-global
+fastcgi_buffer_size 32k;
+fastcgi_send_timeout 300s;
+fastcgi_read_timeout 300s;
+proxy_send_timeout 300s;
+proxy_read_timeout 300s;
+" > ~/.valet/Nginx/00-global
 
 #install the go language and mailhog
 sudo apt -y install golang-go
